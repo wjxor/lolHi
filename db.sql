01fc5587c89b5ef488fd0cf2c633ed0a83716bd3
wjxor <shko940575@gmail.com>
2020-10-10 23:39:34 +0900

    db를 추가하지 못한 것 추가함

---
+++ db.sql
@@ -0,0 +1,26 @@
+# 데이터 베이스 생성
+DROP DATABASE IF EXISTS lolHi;
+CREATE DATABASE lolHi;
+USE lolHi;
+
+# 게시물 테이블 생성
+CREATE TABLE article (
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    title CHAR(200) NOT NULL,
+    `body` TEXT NOT NULL
+);
+
+# 게시물 데이터 생성
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = "제목1",
+`body` = "내용1";
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = "제목2",
+`body` = "내용2";
